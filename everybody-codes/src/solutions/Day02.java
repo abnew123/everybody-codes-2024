b914034c52c40d14d6407625b91a353addf82e21
abnew123 <shadowghosts31415@gmail.com>
2024-11-18 18:21:23 -0500

    Day 2 and 11

--- everybody-codes/src/solutions/Day02.java
+++ everybody-codes/src/solutions/Day02.java
@@ -1,2 +1,148 @@
-package solutions;public class Day02 {
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day02 {
+    public String solve(int part, Scanner in) {
+        int answer = 0;
+        List<String> words = new ArrayList<>(List.of(in.nextLine().split(":")[1].split(",")));
+        in.nextLine();
+        if(part == 1){
+            String[] inscription = in.nextLine().replaceAll("[,.]","").split(" ");
+            for(String word: inscription){
+                for(String powerWord: words){
+                    if(word.contains(powerWord)){
+                        answer++;
+                    }
+                }
+            }
+        }
+        if(part == 2){
+            while(in.hasNext()){
+                answer+= runicSymbols(in.nextLine(), words);
+            }
+        }
+        if(part == 3){
+            List<String> backwardsWords = new ArrayList<>();
+            for(String word: words){
+                backwardsWords.add(new StringBuilder(word).reverse().toString());
+            }
+            words.addAll(backwardsWords);
+            List<String> armor = new ArrayList<>();
+            while(in.hasNext()){
+                armor.add(in.nextLine());
+            }
+            char[][] grid = new char[armor.size()][armor.get(0).length()];
+            for(int i = 0; i < armor.size(); i++){
+                for(int j = 0; j < armor.get(0).length(); j++){
+                    grid[i][j] = armor.get(i).charAt(j);
+                }
+            }
+            boolean[][] runic = new boolean[armor.size()][armor.get(0).length()];
+            for(int i = 0; i < armor.size(); i++){
+                rowHelper(i, grid, runic, words);
+            }
+            for(int j = 0; j < armor.get(0).length(); j++){
+                colHelper(j, grid, runic, words);
+            }
+
+            for(boolean[] row: runic){
+                for(boolean b: row){
+                    if(b){
+                        answer++;
+                    }
+                }
+            }
+
+        }
+
+        return answer+"";
+    }
+
+    private void rowHelper(int i, char[][] grid, boolean[][] runic, List<String> words){
+        for(int j = 0; j < grid[0].length; j++){
+            String word = constructWord(grid, i, j, true);
+            for(String w: words) {
+                if (word.startsWith(w)) {
+                    for(int k = 0; k < w.length(); k++){
+                        runic[i][(j + k) % grid[0].length] = true;
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void colHelper(int j, char[][] grid, boolean[][] runic, List<String> words){
+        String word = constructWord(grid, 0, j, false);
+        for(int i = 0; i < word.length(); i++){
+            String partial = word.substring(i);
+            for(String w: words) {
+                if (partial.startsWith(w)) {
+                    for(int k = 0; k < w.length(); k++){
+                        runic[k + i][j] = true;
+                    }
+                }
+            }
+        }
+
+
+
+    }
+
+    private String constructWord(char[][] grid, int i, int j, boolean horizontal){
+        if(horizontal){
+            String answer = "";
+            for(int k = j; k < grid[0].length + j; k++){
+                answer += grid[i][k%grid[0].length];
+            }
+            return answer;
+        }
+        else{
+            String answer = "";
+            for(int k = i; k < grid.length + i; k++){
+                answer += grid[k % grid.length][j];
+            }
+            return answer;
+        }
+    }
+
+
+
+    private int runicSymbols(String line, List<String> words){
+        int answer = 0;
+        boolean[] runic = new boolean[line.length()];
+        String line2 = new StringBuffer(line).reverse().toString();
+        boolean[] runic2 = new boolean[line.length()];
+        for(int i = 0; i < line.length(); i++){
+            for(String word: words){
+                if(i + word.length() <= line.length() && line.startsWith(word, i)){
+                    for(int j = i; j < i + word.length(); j++){
+                        runic[j] = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < line.length(); i++){
+            for(String word: words){
+                if(i + word.length() <= line.length() && line2.startsWith(word, i)){
+                    for(int j = i; j < i + word.length(); j++){
+                        runic2[j] = true;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < line.length(); i++){
+            if(runic[i] || runic2[line.length() - i - 1]){
+                answer++;
+            }
+        }
+        return answer;
+
+    }
 }
--- everybody-codes/src/solutions/Day11.java
+++ everybody-codes/src/solutions/Day11.java
@@ -1,2 +1,58 @@
-package solutions;public class Day11 {
+package solutions;
+
+import java.util.*;
+
+public class Day11 {
+    public String solve(int part, Scanner in) {
+        long answer = 0;
+        Set<String> possible = new HashSet<>();
+        Map<String, List<String>> growth = new HashMap<>();
+        while(in.hasNext()){
+            String line = in.nextLine();
+            growth.put(line.split(":")[0], List.of(line.split(":")[1].split(",")));
+            possible.addAll(List.of(line.split(":")[1].split(",")));
+        }
+        if(part == 1){
+            answer = numTermites(growth, 4, "A");
+        }
+        if(part == 2){
+            answer = numTermites(growth, 10, "Z");
+        }
+        if(part == 3){
+            long min = Long.MAX_VALUE;
+            long max = Long.MIN_VALUE;
+
+            for(String possibility: possible){
+                long result = numTermites(growth, 20, possibility);
+                min = Math.min(min, result);
+                max = Math.max(max, result);
+            }
+           answer = max - min;
+        }
+
+
+
+        return answer+"";
+    }
+
+    private long numTermites(Map<String, List<String>> growth, int days, String start){
+        Map<String, Long> termites = new HashMap<>();
+        termites.put(start, 1L);
+        int counter = days;
+        while(counter > 0){
+            counter--;
+            Map<String, Long> newTermites = new HashMap<>();
+            for(String termite: termites.keySet()){
+                for(String spawn: growth.get(termite)){
+                    newTermites.put(spawn, newTermites.getOrDefault(spawn,0L) + termites.get(termite));
+                }
+            }
+            termites = newTermites;
+        }
+        long value = 0;
+        for(Long population: termites.values()){
+            value+=population;
+        }
+        return value;
+    }
 }
